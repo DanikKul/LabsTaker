abf1f587dd316382d33463ed14fb0ad63544bc19
Kulakovich Daniil <92942037+DanikKul@users.noreply.github.com>
2023-06-02 14:16:23 +0300

    SUPER COMMIT

--- database.py
+++ database.py
@@ -7,9 +7,19 @@ def database_connect(database_name: str):
     return con, cursor
 
 
-def database_init(con, cursor):
+def tables_database_init(con, cursor):
     cursor.execute("""
-                CREATE TABLE IF NOT EXISTS 'queue'
+                CREATE TABLE IF NOT EXISTS 'tables'
+                    ('db_id' INTEGER PRIMARY KEY AUTOINCREMENT,  
+                    'name' TEXT,
+                    'date' TEXT)
+    """)
+    con.commit()
+
+
+def database_init(con, cursor, name):
+    cursor.execute(f"""
+                CREATE TABLE IF NOT EXISTS '{name}'
                     ('db_id' INTEGER PRIMARY KEY AUTOINCREMENT,  
                     'tg_id' TEXT,
                     'name' TEXT,
@@ -19,19 +29,30 @@ def database_init(con, cursor):
     con.commit()
 
 
-def delete_table(con, cursor):
+def admin_db_init(con, cursor):
     cursor.execute("""
-        DROP TABLE 'queue'
+                CREATE TABLE IF NOT EXISTS 'admins'
+                    ('db_id' INTEGER PRIMARY KEY AUTOINCREMENT,  
+                    'tg_id' TEXT,
+                    'name' TEXT)
     """)
-
-
-def insert_value(con, cursor, value):
-    cursor.execute(f"INSERT INTO 'queue' ('tg_id', 'time', 'name', 'change') VALUES ('{value['tg_id']}', '{value['time']}', '{value['username']}', {value['change']})")
     con.commit()
 
 
-def get_all(con, cursor):
-    cursor.execute("SELECT * from 'queue' ORDER BY 'time'")
+def delete_table(con, cursor, name):
+    cursor.execute(f"""
+        DROP TABLE '{name}'
+    """)
+
+
+def insert_value(con, cursor, value, db_name):
+    cursor.execute(
+        f"INSERT INTO '{db_name}' ('tg_id', 'time', 'name', 'change') VALUES ('{value['tg_id']}', '{value['time']}', '{value['username']}', {value['change']})")
+    con.commit()
+
+
+def get_all(con, cursor, name):
+    cursor.execute(f"SELECT * from '{name}' ORDER BY 'db_id'")
     return cursor.fetchall()
 
 
@@ -40,57 +61,115 @@ def close_connection(con, cursor):
     con.close()
 
 
-def get_status_by_id(con, cursor, id):
-    cursor.execute(f"select * from 'queue' where tg_id = {id} order by 'time'")
+def get_status_by_id(con, cursor, id, db_name):
+    cursor.execute(f"select * from '{db_name}' where tg_id = {id} order by 'time'")
     return cursor.fetchall()
 
 
-def get_status_by_no(con, cursor, id):
-    cursor.execute(f"select * from 'queue' where db_id = {id} order by 'time'")
+def get_status_by_no(con, cursor, id, name):
+    cursor.execute(f"select * from '{name}' where db_id = {id} order by 'time'")
     return cursor.fetchall()
 
 
 def change_queue(con, cursor, db_id1, db_id2):
     try:
-        cursor.execute(f"""UPDATE 'queue' SET tg_id = '{db_id2[1]}', name = '{db_id2[2]}', change = -1 WHERE db_id = {int(db_id1[0])}""")
+        cursor.execute(
+            f"""UPDATE 'queue' SET tg_id = '{db_id2[1]}', name = '{db_id2[2]}', change = -1 WHERE db_id = {int(db_id1[0])}""")
         con.commit()
-        cursor.execute(f"""UPDATE 'queue' SET tg_id = '{db_id1[1]}', name = '{db_id1[2]}', change = -1 WHERE db_id = {int(db_id2[0])}""")
-        con.commit()
-    except Exception as e:
-        print(e)
-
-
-def update_change(con, cursor, db_id, change):
-    try:
-        cursor.execute(f"""UPDATE 'queue' SET tg_id = '{db_id[1]}', name = '{db_id[2]}', time = '{db_id[3]}', change = {int(change)} WHERE db_id = {int(db_id[0])}""")
+        cursor.execute(
+            f"""UPDATE 'queue' SET tg_id = '{db_id1[1]}', name = '{db_id1[2]}', change = -1 WHERE db_id = {int(db_id2[0])}""")
         con.commit()
     except Exception as e:
         print(e)
 
 
-def update_name(con, cursor, tg_id, name):
-    try:
-        cursor.execute(f"""UPDATE 'queue' SET name = '{name}' WHERE tg_id = '{tg_id}'""")
-        con.commit()
-    except Exception as e:
-        print(e)
-
-
-def cancel_take(con, cursor, id):
+def update_change(con, cursor, db_id, change, db_name):
     try:
         cursor.execute(
-            f"""DELETE FROM 'queue' WHERE tg_id = '{id}'"""
+            f"""UPDATE '{db_name}' SET tg_id = '{db_id[1]}', name = '{db_id[2]}', time = '{db_id[3]}', change = {int(change)} WHERE db_id = {int(db_id[0])}""")
+        con.commit()
+    except Exception as e:
+        print(e)
+
+
+def update_name(con, cursor, tg_id, name, db_name):
+    try:
+        cursor.execute(f"""UPDATE '{db_name}' SET name = '{name}' WHERE tg_id = '{tg_id}'""")
+        con.commit()
+    except Exception as e:
+        print(e)
+
+
+def make_admin(con, cursor, value):
+    try:
+        cursor.execute(f"INSERT INTO 'admins' ('tg_id', 'name') VALUES ('{value['tg_id']}', '{value['username']}')")
+        con.commit()
+    except Exception as e:
+        print(e)
+
+
+def is_admin(con, cursor, tg_id):
+    try:
+        cursor.execute(f"SELECT * FROM 'admins' WHERE tg_id = '{tg_id}'")
+        if len(cursor.fetchall()) >= 1:
+            print('true')
+            return True
+        else:
+            print('false')
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def cancel_take(con, cursor, id, name):
+    try:
+        cursor.execute(
+            f"""DELETE FROM '{name}' WHERE tg_id = '{id}'"""
         )
         lst = get_all(con, cursor)
-        delete_table(con, cursor)
-        database_init(con, cursor)
+        delete_table(con, cursor, name)
+        database_init(con, cursor, name)
         if lst:
             for human in lst:
                 insert_value(con, cursor, {'tg_id': human[1], 'time': human[3],
-                                   'username': human[2],
-                                   'change': human[4]})
+                                           'username': human[2],
+                                           'change': human[4]})
         con.commit()
 
+    except Exception as e:
+        print(e)
+
+
+def is_exist_table(con, cursor, name):
+    try:
+        cursor.execute(f"SELECT * FROM 'tables' WHERE name = '{name}'")
+        if len(cursor.fetchall()) >= 1:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_all_tables(con, cursor):
+    cursor.execute("SELECT * from 'tables' ORDER BY 'name'")
+    return cursor.fetchall()
+
+
+def insert_table(con, cursor, value):
+    try:
+        cursor.execute(f"INSERT INTO 'tables' ('name', 'date') VALUES ('{value['name']}', '{value['date']}')")
+        con.commit()
+    except Exception as e:
+        print(e)
+
+
+def delete_table_from_table(con, cursor, name):
+    try:
+        cursor.execute(f"DELETE FROM 'tables' where name = '{name}'")
+        con.commit()
     except Exception as e:
         print(e)
 
@@ -98,5 +177,5 @@ def cancel_take(con, cursor, id):
 """FOR TESTS"""
 if __name__ == "__main__":
     con, cursor = database_connect('queue')
-    database_init(con, cursor)
-    delete_table(con, cursor)
+    database_init(con, cursor, 'queue')
+    delete_table(con, cursor, 'queue')
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ with open('config.json') as fp:
 # Create telebot object
 bot = tt.TeleBot(token=config['token'], parse_mode=None)
 
-# Initialize database
+# Initialize databases
 con, cursor = database_connect(config['db_name'])
-cursor.execute("DROP TABLE IF EXISTS 'queue'")
-database_init(con, cursor)
+admin_db_init(con, cursor)
+tables_database_init(con, cursor)
 close_connection(con, cursor)
 
 
@@ -27,17 +27,36 @@ def handle_start(message):
     bot.send_message(message.chat.id, text='Привет')
     bot.send_message(message.chat.id, text='Вот все команды, которые ты можешь использовать')
     bot.send_message(message.chat.id,
-                     text='/take - Занять очередь\n/status - Посмотреть какой ты по счету в очереди\n/list - Посмотреть всю очередь\n/change - Поменяться местами с кем-либо\n/cancel - Перехотел сдавать\n/edit - Изменить имя в очереди')
+                     text='/take - Занять очередь\n/status - Посмотреть какой ты по счету в очереди\n/list - Посмотреть всю очередь\n/change - Поменяться местами с кем-либо\n/cancel - Перехотел сдавать\n/edit - Изменить имя в очереди\n/queues - Посмотреть список очередей')
+    con_l, cursor_l = database_connect(config['db_name'])
+    if is_admin(con_l, cursor_l, message.chat.id):
+        bot.send_message(message.chat.id,
+                         text='Для админов\n/create - создать очередь\n/delete - удалить очередь')
 
 
 @bot.message_handler(commands=['take'])
 def handle_take(message):
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
+    bot.register_next_step_handler(message, callback_take_handler)
+
+
+def callback_take_handler(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    bot.send_message(message.chat.id, text="Для того чтобы занять очередь, напишите любое сообщение")
+    bot.register_next_step_handler(message, callback_take2_handler, message.text)
+
+
+def callback_take2_handler(message, name):
     time = dt.now()
     con_l, cursor_l = database_connect(config['db_name'])
     # if time.hour < 22 or time.minute < 39:
     #     bot.send_message(message.chat.id, f"Слишком рано: {time.strftime('%H:%M:%S:%f')}")
     #     return
-    lst = get_all(con_l, cursor_l)
+    lst = get_all(con_l, cursor_l, name)
     if lst is not None:
         for human in lst:
             if human[1] == str(message.chat.id):
@@ -47,7 +66,7 @@ def handle_take(message):
     time_s = time.strftime("%H:%M:%S:%f")
     insert_value(con_l, cursor_l, {'tg_id': message.chat.id, 'time': time_s,
                                    'username': f"{message.from_user.first_name if message.from_user.first_name is not None else ''} {message.from_user.last_name if message.from_user.last_name is not None else ''}",
-                                   'change': -1})
+                                   'change': -1}, name)
     close_connection(con_l, cursor_l)
     bot.send_message(message.chat.id,
                      text=f'Время получения запроса: {time_s}')
@@ -55,17 +74,35 @@ def handle_take(message):
 
 @bot.message_handler(commands=['status'])
 def handle_status(message):
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
+    bot.register_next_step_handler(message, callback_status_handler)
+
+
+def callback_status_handler(message):
     con_l, cursor_l = database_connect(config['db_name'])
-    if not get_status_by_id(con_l, cursor_l, message.chat.id):
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    if not get_status_by_id(con_l, cursor_l, message.chat.id, message.text):
         bot.send_message(message.chat.id, f"Никто не занял очередь :(")
     else:
-        bot.send_message(message.chat.id, f"{get_status_by_id(con_l, cursor_l, message.chat.id)[0][0]}")
+        bot.send_message(message.chat.id, f"{get_status_by_id(con_l, cursor_l, message.chat.id, message.text)[0][0]}")
 
 
 @bot.message_handler(commands=['list'])
 def handle_list(message):
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
+    bot.register_next_step_handler(message, callback_list_handler)
+
+
+def callback_list_handler(message):
     con_l, cursor_l = database_connect(config['db_name'])
-    lst = get_all(con_l, cursor_l)
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    lst = get_all(con_l, cursor_l, message.text)
     if not lst:
         bot.send_message(message.chat.id, text="Никто не занял очередь :(")
     else:
@@ -78,16 +115,26 @@ def handle_list(message):
 
 @bot.message_handler(commands=['change'])
 def handle_change(message):
-    bot.send_message(message.chat.id, text='Введите номер в очереди с которым хотите поменяться')
-    bot.register_next_step_handler(message, callback_handler)
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
+    bot.register_next_step_handler(message, callback_change_handler)
 
 
-def callback_handler(message):
+def callback_change_handler(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    bot.send_message(message.chat.id, text=f'Введите номер человека с которым хотите поменяться')
+    bot.register_next_step_handler(message, callback_change2_handler, message.text)
+
+
+def callback_change2_handler(message, name):
     no: int
     try:
         no = int(message.text)
         con_l, cursor_l = database_connect(config['db_name'])
-        lst = get_all(con_l, cursor_l)
+        lst = get_all(con_l, cursor_l, name)
         if not lst:
             bot.send_message(message.chat.id, text="Никто не занял очередь :(")
             return
@@ -95,11 +142,9 @@ def callback_handler(message):
             bot.send_message(message.chat.id, text="Такого номера не существует")
             return
         bot.send_message(message.chat.id, 'Запрос на изменение очереди принят')
-        update_change(con_l, cursor_l, get_status_by_id(con_l, cursor_l, message.chat.id)[0], no)
-        if lst[no - 1][4] == get_status_by_id(con_l, cursor_l, message.chat.id)[0][0] != -1:
-            print(get_status_by_id(con_l, cursor_l, message.chat.id))
-            print(get_status_by_no(con_l, cursor_l, no))
-            change_queue(con_l, cursor_l, get_status_by_id(con_l, cursor_l, message.chat.id)[0], get_status_by_no(con_l, cursor_l, no)[0])
+        update_change(con_l, cursor_l, get_status_by_id(con_l, cursor_l, message.chat.id, name)[0], no, name)
+        if lst[no - 1][4] == get_status_by_id(con_l, cursor_l, message.chat.id, name)[0][0] != -1:
+            change_queue(con_l, cursor_l, get_status_by_id(con_l, cursor_l, message.chat.id, name)[0], get_status_by_no(con_l, cursor_l, no, name)[0])
             bot.send_message(message.chat.id, 'Очередь изменена')
         else:
             bot.send_message(message.chat.id,
@@ -112,24 +157,138 @@ def callback_handler(message):
 
 
 @bot.message_handler(commands=['cancel'])
-def handle_change(message):
+def handle_cancel(message):
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
+    bot.register_next_step_handler(message, callback_cancel_handler)
+
+
+def callback_cancel_handler(message):
     con_l, cursor_l = database_connect(config['db_name'])
-    cancel_take(con_l, cursor_l, message.chat.id)
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    cancel_take(con_l, cursor_l, message.chat.id, message.text)
     close_connection(con_l, cursor_l)
     bot.send_message(message.chat.id, text='Ты освободил свое место в очереди')
 
 
 @bot.message_handler(commands=['edit'])
-def handle_change(message):
-    bot.send_message(message.chat.id, text='Напиши имя и фамилию')
+def handle_edit(message):
+    bot.send_message(message.chat.id, 'Введите имя очереди из доступных в /queues')
     bot.register_next_step_handler(message, callback_edit_handler)
 
 
 def callback_edit_handler(message):
     con_l, cursor_l = database_connect(config['db_name'])
-    update_name(con_l, cursor_l, message.chat.id, message.text)
+    if not is_exist_table(con_l, cursor_l, message.text):
+        close_connection(con_l, cursor_l)
+        bot.send_message(message.chat.id, text=f'Такой очереди нет, посмотрите список доступных очередей в /queues')
+        return
+    close_connection(con_l, cursor_l)
+    bot.send_message(message.chat.id, text='Напиши имя и фамилию')
+    bot.register_next_step_handler(message, callback_edit2_handler, message.text)
+
+
+def callback_edit2_handler(message, name):
+    con_l, cursor_l = database_connect(config['db_name'])
+    update_name(con_l, cursor_l, message.chat.id, message.text, name)
     close_connection(con_l, cursor_l)
     bot.send_message(message.chat.id, text='Готово')
+
+
+@bot.message_handler(commands=['supersecretadmin'])
+def handle_admin(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    if is_admin(con_l, cursor_l, message.chat.id):
+        bot.send_message(message.chat.id, text="Ты уже админ")
+        return
+    bot.send_message(message.chat.id, text="Введи пароль")
+    bot.register_next_step_handler(message, callback_admin_handler)
+    close_connection(con_l, cursor_l)
+
+
+def callback_admin_handler(message):
+    if message.text != config['pass']:
+        bot.send_message(message.chat.id, text="Ты не знаешь пароля ухади!!1!")
+        return
+    con_l, cursor_l = database_connect(config['db_name'])
+    make_admin(con_l, cursor_l, {"tg_id": message.chat.id, "username": f"{message.from_user.first_name if message.from_user.first_name is not None else ''} {message.from_user.last_name if message.from_user.last_name is not None else ''}"})
+    bot.send_message(message.chat.id, text="Ты админ")
+    close_connection(con_l, cursor_l)
+
+
+@bot.message_handler(commands=['create'])
+def handle_create(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    if not is_admin(con_l, cursor_l, message.chat.id):
+        bot.send_message(message.chat.id, text="У тебя недостаточно прав для такой команды")
+        return
+    bot.send_message(message.chat.id, text="Создание очереди")
+    bot.send_message(message.chat.id, text="Введи имя очереди, которую хочешь создать")
+    close_connection(con_l, cursor_l)
+    bot.register_next_step_handler(message, callback_create_handler)
+
+
+def callback_create_handler(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    tables = get_all_tables(con_l, cursor_l)
+    if message.text == 'admins' or message.text == 'tables':
+        bot.send_message(message.chat.id, text='Самый умный что-ли?')
+        return
+    for table in tables:
+        if table[1] == message.text:
+            bot.send_message(message.chat.id, text='Очередь с таким именем уже существует')
+            return
+    database_init(con_l, cursor_l, message.text)
+    insert_table(con_l, cursor_l, {'name': message.text, 'date': dt.now()})
+    bot.send_message(message.chat.id, text=f"Очередь с именем {message.text} создана")
+    close_connection(con_l, cursor_l)
+
+
+@bot.message_handler(commands=['delete'])
+def handle_delete(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    if not is_admin(con_l, cursor_l, message.chat.id):
+        bot.send_message(message.chat.id, text="У тебя недостаточно прав для такой команды")
+        return
+    bot.send_message(message.chat.id, text="Удаление очереди")
+    bot.send_message(message.chat.id, text="Введи имя очереди, которую хочешь удалить")
+    bot.register_next_step_handler(message, callback_delete_handler)
+
+
+def callback_delete_handler(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    tables = get_all_tables(con_l, cursor_l)
+    exist = False
+    if message.text == 'admins' or message.text == 'tables':
+        bot.send_message(message.chat.id, text='Самый умный что-ли?')
+        return
+    for table in tables:
+        if table[1] == message.text:
+            exist = True
+    if not exist:
+        bot.send_message(message.chat.id, text='Очередь с таким именем не существует')
+        return
+    delete_table(con_l, cursor_l, message.text)
+    delete_table_from_table(con_l, cursor_l, message.text)
+    bot.send_message(message.chat.id, text=f"Очередь с именем {message.text} удалена")
+    close_connection(con_l, cursor_l)
+
+
+@bot.message_handler(commands=['queues'])
+def handle_queues(message):
+    con_l, cursor_l = database_connect(config['db_name'])
+    tables = get_all_tables(con_l, cursor_l)
+    if tables:
+        s = 'Очереди:\n'
+        idx = 1
+        for table in tables:
+            s += f"№{str(idx)} {table[1]}"
+        bot.send_message(message.chat.id, text=s)
+    else:
+        bot.send_message(message.chat.id, text="Нет ни одной очереди :(")
+        bot.send_message(message.chat.id, text="Если скоро занимать очередь, а ее нет напишите админам")
 
 
 bot.infinity_polling(timeout=1000, skip_pending=True, long_polling_timeout=1000)
